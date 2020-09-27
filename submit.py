0df49f91cbb7caff58a6932f1b2d0f75b131c5fa
‘changqinyao’ <1142253918@qq.com>
2020-09-27 15:39:31 +0800

    yolo-kaggle

--- submit.py
+++ submit.py
@@ -142,21 +142,21 @@ def detect(save_img=False):
                     raise StopIteration
 
                     # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'images':
-                    cv2.imwrite(save_path, im0)
-                else:
-                    if vid_path != save_path:  # new video
-                        vid_path = save_path
-                        if isinstance(vid_writer, cv2.VideoWriter):
-                            vid_writer.release()  # release previous video writer
-
-                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*opt.fourcc), fps,
-                                                     (w, h))
-                    vid_writer.write(im0)
+            # if save_img:
+            #     if dataset.mode == 'images':
+            #         cv2.imwrite(save_path, im0)
+            #     else:
+            #         if vid_path != save_path:  # new video
+            #             vid_path = save_path
+            #             if isinstance(vid_writer, cv2.VideoWriter):
+            #                 vid_writer.release()  # release previous video writer
+            #
+            #             fps = vid_cap.get(cv2.CAP_PROP_FPS)
+            #             w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            #             h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            #             vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*opt.fourcc), fps,
+            #                                          (w, h))
+            #         vid_writer.write(im0)
 
 
         # i add
